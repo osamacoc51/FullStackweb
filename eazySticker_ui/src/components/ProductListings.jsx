b0970f2c4f431876b16b447fbbd84c92f01f306b
Md Osama <osamacoc51@gmail.com>
2026-08-30 01:39:20 +0530

    added sorting logic

--- eazySticker_ui/src/components/ProductListings.jsx
+++ eazySticker_ui/src/components/ProductListings.jsx
@@ -1,28 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 import ProductCard from "./ProductCard";
 import SearchBox from "./searchBox";
 import DropDown from "./dropDown";
 
-  const sortList = ["Popularity", "Price Low to High", "Price High to Low"];
+const sortList = ["Popularity", "Price Low to High", "Price High to Low"];
 
 export default function ProductListings({products}) {
 
-let searchText = "";
+const [searchText, setSearchText] = useState("");
+const [selectedSort, setSelectedSort] = useState("Popularity");
 
  function handleSearchChange(inputSearch) {
-    searchText=inputSearch;
-    console.log(searchText);
+    setSearchText(inputSearch);
+ }
+
+ function handleSortChange (sortType){
+    setSelectedSort(sortType);
+ }
+
+ let filteredAndSorted = Array.isArray(products)
+   ? products.filter(
+       (product) =>
+         product.name.toLowerCase().includes(searchText.toLowerCase()) ||
+         product.description.toLowerCase().includes(searchText.toLowerCase()),
+     )
+   : [];
+
+ switch (selectedSort) {
+   case "Price Low to High":
+     filteredAndSorted.sort((a, b) => parseFloat(a.price) - parseInt(b.price));
+     break;
+   case "Price High to Low":
+     filteredAndSorted.sort((a, b) => parseFloat(b.price) - parseInt(a.price));
+     break;
+   case "Popularity":
+   default:
+     filteredAndSorted.sort(
+       (a, b) => parseInt(b.popularity) - parseInt(a.popularity),
+     );
+     break;
  }
 
     return (
         <div className="max-w-6xl mx-auto">
             <div className="flex flex-col sm:flex-row justify-between items-center gap-4 pt-12">
                 <SearchBox label="Search" placeholder="Search products..." value={searchText} handleSearch={(value)=>handleSearchChange(value)}/>
-                <DropDown label="Sort By" options={sortList} value="Popularity" />
+                <DropDown label="Sort By" options={sortList} value={selectedSort} handleSort={(value)=>handleSortChange(value)}/>
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
-                {products.length > 0 ? (
-                    products.map((product)=> (
+                {filteredAndSorted.length > 0 ? (
+                    filteredAndSorted.map((product)=> (
                         <ProductCard key={product.productId} product={product}/>
                     ))
                 ) : (
