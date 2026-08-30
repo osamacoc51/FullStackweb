9e6070af3c1d18c6253df8bb1c73f6522da2d0bb
Md Osama <osamacoc51@gmail.com>
2026-08-30 23:42:02 +0530

    added useMemo for caching complex calculation

--- eazySticker_ui/src/components/ProductListings.jsx
+++ eazySticker_ui/src/components/ProductListings.jsx
@@ -1,62 +1,72 @@
-import React, {useState} from "react";
+import React, { useState, useMemo } from "react";
 import ProductCard from "./ProductCard";
 import SearchBox from "./searchBox";
 import DropDown from "./dropDown";
 
 const sortList = ["Popularity", "Price Low to High", "Price High to Low"];
 
-export default function ProductListings({products}) {
+export default function ProductListings({ products }) {
+  const [searchText, setSearchText] = useState("");
+  const [selectedSort, setSelectedSort] = useState("Popularity");
 
-const [searchText, setSearchText] = useState("");
-const [selectedSort, setSelectedSort] = useState("Popularity");
+  const filteredAndSortedProducts = useMemo(() => {
+    if (!Array.isArray(products)) {
+      return [];
+    }
+    let filteredProducts = products.filter(
+      (product) =>
+        product.name.toLowerCase().includes(searchText.toLowerCase()) ||
+        product.description.toLowerCase().includes(searchText.toLowerCase()),
+    );
 
- function handleSearchChange(inputSearch) {
+    return filteredProducts.slice().sort((a, b) => {
+      switch (selectedSort) {
+        case "Price Low to High":
+          return parseFloat(a.price) - parseFloat(b.price);
+        case "Price High to Low":
+          return parseFloat(b.price) - parseFloat(a.price);
+        case "Popularity":
+        default:
+          return parseInt(b.popularity) - parseInt(a.popularity);
+      }
+    });
+  }, [products, searchText, selectedSort]);
+
+  function handleSearchChange(inputSearch) {
     setSearchText(inputSearch);
- }
+  }
 
- function handleSortChange (sortType){
+  function handleSortChange(sortType) {
     setSelectedSort(sortType);
- }
+  }
 
- let filteredAndSorted = Array.isArray(products)
-   ? products.filter(
-       (product) =>
-         product.name.toLowerCase().includes(searchText.toLowerCase()) ||
-         product.description.toLowerCase().includes(searchText.toLowerCase()),
-     )
-   : [];
-
- switch (selectedSort) {
-   case "Price Low to High":
-     filteredAndSorted.sort((a, b) => parseFloat(a.price) - parseInt(b.price));
-     break;
-   case "Price High to Low":
-     filteredAndSorted.sort((a, b) => parseFloat(b.price) - parseInt(a.price));
-     break;
-   case "Popularity":
-   default:
-     filteredAndSorted.sort(
-       (a, b) => parseInt(b.popularity) - parseInt(a.popularity),
-     );
-     break;
- }
-
-    return (
-        <div className="max-w-6xl mx-auto">
-            <div className="flex flex-col sm:flex-row justify-between items-center gap-4 pt-12">
-                <SearchBox label="Search" placeholder="Search products..." value={searchText} handleSearch={(value)=>handleSearchChange(value)}/>
-                <DropDown label="Sort By" options={sortList} value={selectedSort} handleSort={(value)=>handleSortChange(value)}/>
-            </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
-                {filteredAndSorted.length > 0 ? (
-                    filteredAndSorted.map((product)=> (
-                        <ProductCard key={product.productId} product={product}/>
-                    ))
-                ) : (
-                    <p className="text-center font-primary font-bold text-lg text-primary">No products found.</p>
-                )
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className="max-w-6xl mx-auto">
+      <div className="flex flex-col sm:flex-row justify-between items-center gap-4 pt-12">
+        <SearchBox
+          label="Search"
+          placeholder="Search products..."
+          value={searchText}
+          handleSearch={(value) => handleSearchChange(value)}
+        />
+        <DropDown
+          label="Sort By"
+          options={sortList}
+          value={selectedSort}
+          handleSort={(value) => handleSortChange(value)}
+        />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
+        {filteredAndSortedProducts.length > 0 ? (
+          filteredAndSortedProducts.map((product) => (
+            <ProductCard key={product.productId} product={product} />
+          ))
+        ) : (
+          <p className="text-center font-primary font-bold text-lg text-primary">
+            No products found.
+          </p>
+        )}
+      </div>
+    </div>
+  );
 }
